bddef42d2fb4910e03cbe9abd6e884e625ec64c0
Aya Jamal <63825294+ayagamal-tech@users.noreply.github.com>
2025-12-20 01:31:56 +0200

    Used String.format

--- src/com/app/Square.java
+++ src/com/app/Square.java
@@ -4,7 +4,6 @@ public class Square implements Shape {
 
     @Override
     public void getArea(double length) {
-        System.out.println("the area of the Square = " + Math.pow(length,2));
+        System.out.println(String.format("the area of the Square = %s", Math.pow(length,2)));
     }
-
 }
