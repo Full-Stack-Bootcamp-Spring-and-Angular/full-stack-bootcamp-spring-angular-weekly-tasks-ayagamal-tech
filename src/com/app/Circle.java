ba32ba2e4a82682609dd7c7bfbea6ec281eda8d2
Aya Jamal <63825294+ayagamal-tech@users.noreply.github.com>
2025-12-20 01:32:52 +0200

    Used String.format for Circle Output

--- src/com/app/Circle.java
+++ src/com/app/Circle.java
@@ -3,8 +3,8 @@ package com.app;
 public class Circle implements Shape {
 
     @Override
-    public void getArea(double redis) {
-        System.out.println("the area of the circle = "+ Math.PI * Math.pow(redis,2));
+    public void getArea(double radius) {
+        System.out.println(String.format("the area of the circle = %s", Math.PI * Math.pow(radius,2)));
     }
 
 }
