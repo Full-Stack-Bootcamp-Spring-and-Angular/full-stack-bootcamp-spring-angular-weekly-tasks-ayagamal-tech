b749c5c647f6cef911763da135b98ca52e7aa3bf
Aya Jamal <63825294+ayagamal-tech@users.noreply.github.com>
2025-12-20 01:29:53 +0200

    Updated try (Scanner input = new Scanner(System.in))

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -7,20 +7,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter: ");
-        double value = input.nextDouble();
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter: ");
+            double value = input.nextDouble();
 
-        // define container
-        ApplicationContext container = new ClassPathXmlApplicationContext("applicationContext.xml");
+            // define container
+            ApplicationContext container = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // retrieve a bean for com.com.app.Circle class
-        Shape circle = container.getBean("circle", Shape.class);
-        circle.getArea(value);
+            // retrieve a bean for Circle class
+            Shape circle = container.getBean("circle", Shape.class);
+            circle.getArea(value);
 
-        // retrieve a bean for com.com.app.Circle class
-        com.app.Shape square = container.getBean("square", com.app.Shape.class);
-        square.getArea(value);
+            // retrieve a bean for Square class
+            Shape square = container.getBean("square", Shape.class);
+            square.getArea(value);
 
+        }
     }
 }
